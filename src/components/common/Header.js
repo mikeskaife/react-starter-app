c54d321b2880c23fc469c56692b5f52e8f52e4b8
mikeskaife <mike@skaife.me>
2018-03-08 15:32:37 +0000

    Create sample Header component

---
+++ src/components/common/Header.js
@@ -0,0 +1,14 @@
+import React, { PropTypes } from 'react';
+import { Link, IndexLink } from 'react-router';
+
+const Header = () => {
+  return (
+    <nav>
+      <IndexLink to="/">Home</IndexLink>
+      {" | "}
+      <Link to="about">About</Link>
+    </nav>
+  );
+};
+
+export default Header;
